03461ba06c3af4d01361210cc02cf18d81e3c1e8
Oliver Quaye <oliver.quaye@amalitech.org>
2024-04-23 14:21:58 -0700

    Login & Signup Authentication

--- src/main.js
+++ src/main.js
@@ -5,8 +5,6 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCircleCheck  } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-// import { createToast } from 'vue3-toastify';
-// import 'vue3-toastify/dist/vue3-toastify.css';
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
@@ -18,7 +16,6 @@ library.add(faCircleCheck );
 
 createApp(App)
   .use(router)
-  // .use(createToast)
   .component('font-awesome-icon', FontAwesomeIcon)
   .use(store)
   .use(vuetify)
